0434e9d1b1a0c2dc3d622392703dae9edd0edf19
Mahe <admin@example.com>
2018-12-10 19:40:55 +0000

    Catch up commit

--- js/connect.js
+++ js/connect.js
@@ -3,13 +3,6 @@ var knownDevices = {}
 var savedDevices = {}
 var deviceToSave;
 
-var config = {
-    serviceUUID: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E",
-    txCharacteristic: "6E400002-B5A3-F393-E0A9-E50E24DCCA9E", // transmit is from the phone's perspective
-    rxCharacteristic: "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"  // receive is from the phone's perspective
-};
-
-
 $('#addDevice').on("click", function(){  
     $('#devices').html("");  
     searchDevices();
